da5cae5436d53ae118a4aa105385a619d4a96d29
Deslate <deslate@outlook.com>
2021-01-07 16:40:17 +0800

    完成了文件列表和文件内容提供

--- api/views.py
+++ api/views.py
@@ -15,17 +15,34 @@ def listContent(request):
     target = request.GET.get("target","/")
     target_path = repo_path+target
     for path,dir_list,file_list in os.walk(target_path):
+        
         if path==target_path:
-            dir_list.remove(".git")
+            try:dir_list.remove(".git")
+            except:pass
             # 只保留 .md 文件
+            print(file_list)
+            md_list = []
             for file_name in file_list:
-                if file_name[-3:-1] != ".md":
-                    file_list.remove(file_name)
-                    
+                print(file_name)
+                if file_name.split(".")[-1] =="md":
+                    md_list.append(file_name)
+                else: print (file_name[-3:])
             result = {
+                "target":target,
                 "dir_list":dir_list,
-                "passage_list":file_list
+                "passage_list":md_list,
             }
     print(result)
-    
-    return JsonResponse({})
+    return JsonResponse(result)
+
+def getPassage(request):
+    repo_path = REPO[0]
+    target = request.GET.get("target","/README.md")
+    target_path = repo_path+target
+    f = open(target_path)
+    lines = f.read()
+    print(lines)
+    return JsonResponse({
+        "target":target,
+        "content":lines,
+    })
